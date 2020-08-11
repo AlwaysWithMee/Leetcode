b42ce6dbf2491d288de93f54a81c31c05d9ce17b
Lucas <lucasyou210@gmail.com>
2020-08-10 20:41:49 -0500

    Leetcode171

---
+++ src/Math/Leetcode171.java
@@ -0,0 +1,14 @@
+package Math;
+
+// 171. Excel Sheet Column Number
+public class Leetcode171 {
+
+    public int titleToNumber(String s) {
+        int res = 0;
+
+        for(int i = 0; i < s.length(); ++i)
+            res = res * 26 + s.charAt(i) - 'A' + 1;
+
+        return res;
+    }
+}

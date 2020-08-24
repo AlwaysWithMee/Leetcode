79d3997fce15f62775e832b33dea0d5c8eaca72e
Lucas <lucasyou210@gmail.com>
2020-08-24 13:42:52 -0500

    Leetcode34

--- src/BinarySearch/Leetcode34.java
+++ src/BinarySearch/Leetcode34.java
@@ -3,6 +3,7 @@ package BinarySearch;
 // 34. Find First and Last Position of Element in Sorted Array
 public class Leetcode34 {
 
+    // [Left, Right)
     public int[] searchRange(int[] nums, int target) {
 
         int res [] = new int [2];
@@ -37,7 +38,6 @@ public class Leetcode34 {
         else
             res[0] = left;
 
-        left = 0;
         right = nums.length;
 
         // Find the right boundary
@@ -54,7 +54,63 @@ public class Leetcode34 {
                 right = mid;
             }
         }
-        res[1] = left - 1;
+
+        res[1] = right - 1;
+        return res;
+    }
+
+    // [Left, Right]
+    public int[] searchRange1(int[] nums, int target) {
+
+        int [] res = new int [2];
+
+        if(nums.length == 0){
+            res[0] = -1;
+            res[1] = -1;
+            return res;
+        }
+
+        // Find the left boundary
+        int left = 0, right = nums.length - 1;
+
+        while(left <= right){
+
+            int mid = left + (right - left) / 2;
+            if(nums[mid] == target)
+                right = mid - 1;
+            else if (nums[mid] > target)
+                right = mid - 1;
+            else if (nums[mid] < target)
+                left = mid + 1;
+        }
+
+        if(left >= nums.length || nums[left] != target){
+            res[0] = -1;
+            res[1] = -1;
+            return res;
+        }
+        else if(nums[left] == target)
+            res[0] = left;
+
+        // Find the right boundary
+        right = nums.length - 1;
+
+        while(left <= right){
+
+            int mid = left + (right - left) / 2;
+            if(nums[mid] == target)
+                left = mid + 1;
+            else if (nums[mid] < target)
+                left = mid + 1;
+            else if (nums[mid] > target)
+                right = mid - 1;
+        }
+
+        if (right < nums.length || res[right] == target)
+            res[1] = right;
+        else
+            res[1] = right - 1;
+
         return res;
     }
 }

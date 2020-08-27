970609c9ea2c63019aab1b22beead9f7b680fa32
Lucas <lucasyou210@gmail.com>
2020-08-26 21:55:25 -0500

    Leetcode69

---
+++ src/BinarySearch/Leetcode69.java
@@ -0,0 +1,25 @@
+package BinarySearch;
+
+// 69. Sqrt(x)
+public class Leetcode69 {
+
+    public int mySqrt(int x) {
+
+        long left = 1, right = x;
+
+        while(left < right){
+
+            long mid = left + (right - left) / 2;
+
+            if(mid * mid < x)
+                left = mid + 1;
+            else
+                right = mid;
+        }
+        if (left * left <= x)
+            return (int)left;
+        else
+            return (int)left-1;
+
+    }
+}

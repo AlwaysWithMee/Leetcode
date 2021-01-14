97c85fffe8c5df608dbc7b4f1049eca42fd9c726
Lucas <lucasyou210@gmail.com>
2021-01-13 22:14:16 -0600

    Leetcode965

---
+++ src/Tree/Leetcode617.java
@@ -0,0 +1,37 @@
+package Tree;
+
+// 617. Merge Two Binary Trees
+public class Leetcode617 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+
+        if (t1 == null && t2 == null) return null;
+        int val = (t1 == null ? 0 : t1.val) + (t2 == null ? 0 : t2.val);
+
+        TreeNode root = new TreeNode(val);
+        root.left = mergeTrees(t1 == null ? null : t1.left, t2 == null ? null : t2.left);
+        root.right = mergeTrees(t1 == null ? null : t1.right, t2 == null ? null : t2.right);
+
+        return root;
+    }
+
+}

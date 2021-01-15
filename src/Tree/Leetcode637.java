4ec1144a452581245252b4ad13d2c45cd068d76a
Lucas <lucasyou210@gmail.com>
2021-01-14 23:15:59 -0600

    Leetcode637

---
+++ src/Tree/Leetcode637.java
@@ -0,0 +1,55 @@
+package Tree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+// 637. Average of Levels in Binary Tree
+public class Leetcode637 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    List<Double> res = new ArrayList<>();
+    public List<Double> averageOfLevels(TreeNode root) {
+
+        if (root == null) return res;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        queue.offer(root);
+
+        while (!queue.isEmpty()){
+
+            int size = queue.size();
+            double sum = 0.0;
+
+            for(int i = 0; i < size; i++){
+
+                TreeNode curr = queue.poll();
+                sum += curr.val;
+
+                if (curr.left != null) queue.offer(curr.left);
+                if (curr.right != null) queue.offer(curr.right);
+
+            }
+            res.add(sum / size);
+
+        }
+
+        return res;
+
+    }
+
+}

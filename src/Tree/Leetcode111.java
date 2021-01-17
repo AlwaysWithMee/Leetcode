956f3fba443731395457011e0d31881f99a5454f
Lucas <lucasyou210@gmail.com>
2021-01-17 09:33:47 -0600

    Leetcode111

--- src/Tree/Leetcode111.java
+++ src/Tree/Leetcode111.java
@@ -11,32 +11,35 @@ public class Leetcode111 {
 
         if (root == null) return 0;
 
-        int depth = 1;
-        Queue<TreeNode> queue = new LinkedList<>();
+        int res = 0;
+
+        Queue <TreeNode> queue = new LinkedList<>();
+
         queue.offer(root);
 
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()){
 
             int size = queue.size();
+            res++;
 
             for(int i = 0; i < size; i++){
 
                 TreeNode curr = queue.poll();
 
-                if(curr.left == null && curr.right == null)
-                    return depth;
+                if (curr.left == null && curr.right == null)
+                    return res;
 
-                if(curr.left != null)
+                if (curr.left != null) {
                     queue.offer(curr.left);
-                if(curr.right != null)
+                }
+                if (curr.right != null) {
                     queue.offer(curr.right);
+                }
 
             }
-
-            depth++;
         }
 
-        return depth;
+        return res;
 
     }
 }

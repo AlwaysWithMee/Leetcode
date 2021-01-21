9b48310288a09adad256b37135c885821f01537d
Lucas <lucasyou210@gmail.com>
2021-01-20 21:41:07 -0600

    Leetcode701

---
+++ src/Tree/Leetcode701.java
@@ -0,0 +1,19 @@
+package Tree;
+
+// 701. Insert into a Binary Search Tree
+public class Leetcode701 {
+
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+
+        if(root == null) return new TreeNode(val);
+
+        if(root.val > val)
+            root.left = insertIntoBST(root.left, val);
+        else
+            root.right = insertIntoBST(root.right, val);
+
+        return root;
+
+    }
+
+}

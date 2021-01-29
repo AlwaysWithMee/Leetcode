88c3ca0209a6b7d0c107f46531a3ab6626c4ced9
Lucas <lucasyou210@gmail.com>
2021-01-28 18:21:59 -0600

    Leetcode94

--- src/Tree/Leetcode94.java
+++ src/Tree/Leetcode94.java
@@ -2,11 +2,14 @@ package Tree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 // 94. Binary Tree Inorder Traversal
 public class Leetcode94 {
 
     List<Integer> res = new ArrayList<>();
+
+    // Recursive Solution
     public List<Integer> inorderTraversal(TreeNode root) {
 
         if(root == null) return res;
@@ -20,4 +23,24 @@ public class Leetcode94 {
         return res;
     }
 
+    // Iterative Solution
+    public List<Integer> inorderTraversal1(TreeNode root) {
+
+        if(root == null) return res;
+        Stack<TreeNode> stack = new Stack<>();
+
+        while(root != null || !stack.isEmpty()){
+
+            while(root != null){
+                stack.push(root);
+                root = root.left;
+            }
+
+            root = stack.pop();
+            res.add(root.val);
+            root = root.right;
+
+        }
+        return res;
+    }
 }

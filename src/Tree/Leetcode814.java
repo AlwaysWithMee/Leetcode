615f805da76e1983d12c93d89ec3b4933a1d9ab6
Lucas <lucasyou210@gmail.com>
2021-02-08 14:19:05 -0600

    Leetcode814

---
+++ src/Tree/Leetcode814.java
@@ -0,0 +1,16 @@
+package Tree;
+
+// 814. Binary Tree Pruning
+public class Leetcode814 {
+
+    public TreeNode pruneTree(TreeNode root) {
+
+        if(root == null) return null;
+        root.left = pruneTree(root.left);
+        root.right = pruneTree(root.right);
+
+        if(root.left == null && root.right == null && root.val == 0) return null;
+        return root;
+
+    }
+}
